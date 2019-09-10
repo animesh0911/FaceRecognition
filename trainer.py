ef45deb8b5fc29b58e3f88e40c29846338c790ed
Animesh Landge <anni.landge@gmail.com>
2019-09-10 23:58:49 +0530

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,35 @@
+#Train a Local Binary Patterns (LBPH) model
+#from the labelled images in /dataset
+#Save the model to a file.
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+path = 'dataset'
+
+def processDataset(path):
+	imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+	print(imagePaths)
+	faceData = []
+	IDs = []
+	for imagePath in imagePaths:
+		if(imagePath == 'dataset/.gitkeep'):
+			continue
+		if(imagePath == 'dataset/.DS_Store'):
+			continue
+		faceimage = Image.open(imagePath).convert('L')
+		faceimageNParray = np.array(faceimage,'uint8')
+		ID = int(os.path.split(imagePath)[-1].split('.')[1])
+		faceData.append(faceimageNParray)
+		print(ID)
+		IDs.append(ID)
+		cv2.imshow("training",faceimageNParray)
+		cv2.waitKey(10)
+	return IDs,faceData
+
+IDs, faces=processDataset(path)
+recognizer.train(faces,np.array(IDs))
+recognizer.save('LBPHfacemodel.yml')
+cv2.destroyAllWindows()
